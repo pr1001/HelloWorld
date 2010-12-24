e7765fed6977d9cbc9cccdd7db7cea88e2bee59d
Peter Robinett <peter@bubblefoundry.com>
2010-12-24 15:53:25 +0100

    Declare the Template with the SiteMap Menu, since Lift doesn't seem to find the HTML file version.

--- src/main/scala/bootstrap/lift_modules/HelloWorld.scala
+++ src/main/scala/bootstrap/lift_modules/HelloWorld.scala
@@ -3,11 +3,17 @@ package HelloWorld {
 
 import net.liftweb.sitemap.{Menu, SiteMap}
 import net.liftweb.http.{LiftRules, S}
+import net.liftweb.sitemap.Loc.Template 
 
 object HelloWorld {
     def init() {
         LiftRules.addToPackages("net.lift_modules.HelloWorld")
-        val mutator = SiteMap.addMenusAtEndMutator(Menu(S ? "Hello World") / "helloworld" :: Nil)
+        val menu = Menu(S ? "Hello World") / "helloworld" >> Template(() => {
+            <lift:surround with="default" at="content">
+                <lift:HelloWorld.sayHello />
+            </lift:surround>
+        })
+        val mutator = SiteMap.addMenusAtEndMutator(menu :: Nil)
         LiftRules.siteMap.foreach(sitemap => LiftRules.setSiteMap(mutator(sitemap)))
     }
 }
